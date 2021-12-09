2f7ad1dcf28fc59b6bd10ab228e27ded561f9812
Richard Janssen <rxpjanssen@hotmail.com>
2021-12-09 18:06:24 +0100

    wires are now flexible Their points move together with the objects they are attached to

--- Assets/Scripts/Wire.cs
+++ Assets/Scripts/Wire.cs
@@ -7,11 +7,24 @@ public class Wire : MonoBehaviour
 {
     public WirePoint startPoint;
     public WirePoint endPoint;
+    private const int PositionCount = 2;
+    private LineRenderer _lineRenderer;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
 
     private void OnDestroy()
     {
         GameObject.Find("WireManager").GetComponent<WireManager>().WireDeleted(this);
         //if(startPoint) startPoint.wireConnections.Remove(this);
         //if(endPoint) endPoint.wireConnections.Remove(this);
+    }
+
+    private void Update()
+    {
+        _lineRenderer.SetPositions(new Vector3[PositionCount]
+            {startPoint.gameObject.transform.position, endPoint.gameObject.transform.position});
     }
 }
